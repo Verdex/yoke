7fd4945c03bf922fb0fdedad8e7d73ecd0858bb3
David L. Inman <davidlevere@gmail.com>
2024-01-10 23:44:23 -0500

    some todos

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -4,3 +4,12 @@ use crate::data::{ Lexeme, Ast, ParseError };
 fn parse(input : Vec<Lexeme>) -> Result<Ast, ParseError> {
     todo!()
 }
+
+// TODO 
+// * parse all brackets with stuff inside
+// * let / assignment type of things
+// * function type things
+// * loopish type of things
+// * statement type of things
+// * dot stuff
+// * function call stuff 
