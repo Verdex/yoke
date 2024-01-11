9fd4acdf28f02effa2107036791468fc67024c7b
David L. Inman <davidlevere@gmail.com>
2024-01-11 18:30:26 -0500

    inc prog parser

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,8 +1,32 @@
 
 use crate::data::{ Lexeme, Ast, ParseError };
 
-fn parse(input : Vec<Lexeme>) -> Result<Ast, ParseError> {
-    todo!()
+enum Type {
+    Paren,
+    Angle,
+    Curl,
+    Square,
+}
+
+pub fn parse(input : Vec<Lexeme>) -> Result<Vec<Ast>, ParseError> {
+    let mut input = input.into_iter();
+
+    let mut ret = vec![];
+    loop {
+        match input.next() {
+            Some(Lexeme::LParen(m)) => {
+                let item = parse_bracket(Type::Paren, &mut input)?;
+                ret.push(item);
+            }
+        }
+    }
+
+    Ok(ret)
+}
+
+
+fn parse_bracket(t : Type, input : &mut impl Iterator<Item = Lexeme>) -> Result<Ast, ParseError> {
+
 }
 
 // TODO 
