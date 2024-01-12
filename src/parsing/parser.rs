63471893ceabf8698230991fbf20ba29e7569431
David L. Inman <davidlevere@gmail.com>
2024-01-11 19:37:50 -0500

    inc prog parser

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -17,7 +17,21 @@ pub fn parse(input : Vec<Lexeme>) -> Result<Vec<Ast>, ParseError> {
             Some(Lexeme::LParen(m)) => {
                 let item = parse_bracket(Type::Paren, &mut input)?;
                 ret.push(item);
-            }
+            },
+            Some(Lexeme::LAngle(m)) => {
+                let item = parse_bracket(Type::Angle, &mut input)?;
+                ret.push(item);
+            },
+            Some(Lexeme::LCurl(m)) => {
+                let item = parse_bracket(Type::Curl, &mut input)?;
+                ret.push(item);
+            },
+            Some(Lexeme::LSquare(m)) => {
+                let item = parse_bracket(Type::Square, &mut input)?;
+                ret.push(item);
+            },
+            Some(l) => { ret.push(Ast::Lex(l)); },
+            None => { break; },
         }
     }
 
@@ -27,6 +41,7 @@ pub fn parse(input : Vec<Lexeme>) -> Result<Vec<Ast>, ParseError> {
 
 fn parse_bracket(t : Type, input : &mut impl Iterator<Item = Lexeme>) -> Result<Ast, ParseError> {
 
+    todo!()
 }
 
 // TODO 
