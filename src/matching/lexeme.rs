d5b0785dbb40c2910d3de45380efd36068fe9632
David L. Inman <davidlevere@gmail.com>
2024-01-13 11:24:42 -0500

    inc prog grouper

--- src/matching/lexeme.rs
+++ src/matching/lexeme.rs
@@ -1,18 +1,35 @@
 
-use crate::data::Lexeme;
+use crate::data::{LMeta, Lexeme};
 
 pub struct LexGrouper<T> {
     input : T,
     pattern : Vec<Pattern>,
     label : String,
+    match_buffer : Vec<Lexeme>,
 }
 
 impl<T : Iterator<Item = Lexeme>> Iterator for LexGrouper<T> {
     type Item = Lexeme;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        match self.input.next() {
+            None => { },
+            Some(l) => { self.match_buffer.push(l); },
+        }
+        if pattern_match(&self.pattern, &self.match_buffer) {
+            let ls = std::mem::replace(&mut self.match_buffer, vec![]);
+            let start = ls.first().unwrap().meta().start;
+            let end = ls.last().unwrap().meta().end;
+            Some(Lexeme::Group(LMeta::multi(start, end), self.label.clone(), ls))
+        }
+        else {
+            todo!()
+        }
     }
+}
+
+fn pattern_match(pattern : &[Pattern], data : &[Lexeme]) -> bool {
+    todo!()
 }
 
 #[derive(Debug)]
@@ -23,5 +40,6 @@ pub enum Pattern {
 }
 
 pub fn group<T : Iterator<Item = Lexeme>>(label : String, pattern : Vec<Pattern>, input : T) -> LexGrouper<T> { 
-    LexGrouper { input, pattern, label }
+    // TODO don't allow empty patterns
+    LexGrouper { input, pattern, label, match_buffer: vec![] }
 }
