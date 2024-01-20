75df60c47dc788888ed925a35938eb959d6d9358
David L. Inman <davidlevere@gmail.com>
2024-01-20 09:54:38 -0500

    inc prog for brakcet process

---
+++ src/matching/bracket.rs
@@ -0,0 +1,45 @@
+
+use crate::data::Bracket;
+
+#[derive(Debug)]
+pub enum Pattern {
+    Wild,
+    Exact(Bracket),
+    Pred(fn(&Bracket) -> bool),
+}
+
+fn pattern_match(pattern : &[Pattern], data : &[Bracket]) -> bool {
+    let pds = pattern.iter().zip(data.iter());
+    for pd in pds {
+        match pd {
+            (Pattern::Wild, _) => { },
+            (Pattern::Pred(f), d) if f(d) => { },
+            (Pattern::Exact(l), d) if l.lmatch(d) => { },
+            _ => { return false; },
+        }
+    }
+    true
+}
+
+pub struct Rule<T> {
+    pattern : Vec<Pattern>, 
+    f : fn(&[Bracket]) -> T,
+}
+
+impl<T> Rule<T> {
+    pub fn new(pattern : Vec<Pattern>, f : fn(&[Bracket]) -> T) -> Rule<T> {
+        Rule { pattern, f }
+    }
+}
+
+impl<T> From<(Vec<Pattern>, fn(&[Bracket]) -> T)> for Rule<T> {
+    fn from(value : (Vec<Pattern>, fn(&[Bracket]) -> T)) ->  Self {
+        Rule::new(value.0, value.1)
+    }
+}
+
+pub fn process<T, I : Iterator<Item = Bracket>>(rules : &[Rule<T>], input : I)  {
+
+    
+        
+}
